a32f59834dda9f863f136189c5cc16e2b300d227
Walid <f0jckejd@proton.me>
2026-07-19 00:12:02 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,38 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(req: Request): Promise<Response> {
+  const upgrade = req.headers.get("upgrade") || "";
+
+  if (upgrade.toLowerCase() !== "websocket") {
+    return new Response("Only WebSocket supported", {
+      status: 400,
+    });
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  socket.onopen = () => {
+    const target = new WebSocket(
+      "wss://fr.connfull.org:9443/stream"
+    );
+
+    target.onopen = () => {
+      socket.onmessage = (e) => target.send(e.data);
+      target.onmessage = (e) => socket.send(e.data);
+    };
+
+    target.onerror = () => {
+      socket.close(1011, "Target error");
+    };
+
+    target.onclose = () => {
+      socket.close(1000, "Target closed");
+    };
+  };
+
+  socket.onerror = () => {};
+
+  return response;
+}
